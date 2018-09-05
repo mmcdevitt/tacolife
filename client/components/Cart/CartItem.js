59432a453ee0098407b7e24ed862cc6019844bbe
Michael McDevitt <mmcdevi1@gmail.com>
2018-09-04 21:30:37 -0400

    Feature: allow user to delete cart items

--- client/components/Cart/CartItem.js
+++ client/components/Cart/CartItem.js
@@ -1,25 +1,38 @@
 import React from 'react';
+import {connect} from 'react-redux'
+import { deleteItem, addQuantity } from '../../store/cartReducer'
 
 class CartItem extends React.Component {
   render () {
-    const {item} = this.props;
+    const {
+      item,
+      deleteItem,
+    } = this.props;
 
     return (
       <div className="cart-item flex space-between">
         <div className="cart-item-details">
           <div className="cart-item-name">
-            {item.menuItem.name}
+            {item.quantity} {item.menuItem.name}
           </div>
           <span className="text-muted text-small">
             Soft shells, Tabasco
           </span>
         </div>
-        <div className="cart-item-price">
-          ${item.menuItem.price}
+        <div className="flex">
+          <div className="cart-item-price">
+            ${item.menuItem.price}
+          </div>
+          <div className="remove-cart-item" onClick={() => deleteItem(item.id, null)}>
+            <i class="fa fa-close"></i>
+          </div>
         </div>
       </div>
     )
   }
 }
 
-export default CartItem;
+export default connect(null, {
+  deleteItem,
+  addQuantity
+})(CartItem)
