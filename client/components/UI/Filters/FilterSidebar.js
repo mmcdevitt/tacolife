17aa45555b31dc0b757d3ad13ca57de517037359
Michael McDevitt <mmcdevi1@gmail.com>
2018-09-23 19:04:50 -0400

    style basic filters sidebar

---
+++ client/components/UI/Filters/FilterSidebar.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import Aside from '../Aside/Aside';
+import Button from '../Button/Button';
+import Slider from '../RangeSlider/Slider';
+
+import './Filters.css';
+
+class FilterSidebar extends React.Component {
+  render () {
+    return (
+      <Aside>
+        <div className="wrapper">
+          <div className="filters">
+
+            <div className="filters-group">
+              <div className="filters-header">
+                <h4>Filters</h4>
+              </div>
+            </div>
+
+            <div className="filters-group">
+              <div className="filters-order-method">
+                <div className="filters-order-method-title">
+                  <h6>I want</h6>
+                </div>
+                <div className="filters">
+                  <div className="btn-group btn-group-block">
+                    <Button block primary className="active">Delivery</Button>
+                    <Button block primary>Pickup</Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="filters-group">
+              <div className="filters-price">
+                <div className="filters-price-title">
+                  <h6>Price</h6>
+                </div>
+                <Slider min={0} max={5} />
+              </div>
+            </div>
+
+            <div className="filters-group">
+              <div className="filters-rating">
+                <div className="filters-rating-title">
+                  <h6>Rating</h6>
+                </div>
+                <Slider min={0} max={5} />
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </Aside>
+    )
+  }
+}
+
+export default FilterSidebar;
